bd69ba9d11dff12755817dfbae951e44bb25a9c7
Fabio Calero <fabiojcalero@gmail.com>
2024-03-13 19:10:04 -0400

    Debugging info added

--- src/passport/index.js
+++ src/passport/index.js
@@ -23,8 +23,10 @@ module.exports = function () {
     done(null, user.id);
   });
   passport.deserializeUser(async (id, done) => {
+    console.log("We are running deserializeUser");
     try {
       const user = await User.findOne({ _id: id });
+      console.log(user);
       return done(null, user);
     } catch (e) {
       console.error(e);
